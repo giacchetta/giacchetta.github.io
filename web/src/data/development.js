9749f5d4e57e603a3f156a57dce41b6ab54dccfe
L.D Giacchetta <giacchetta-@outlook.com>
2024-08-29 10:55:02 -0300

    Fix #13

---
+++ web/src/data/development.js
@@ -0,0 +1,44 @@
+export const development = [
+    {
+      product: 'Google',
+      type: 'Applied Skill',
+      typelnk: '#',
+      name: 'Firebase Framework',
+      namelnk: '#',
+    },
+    {
+      product: 'JavaScript',
+      type: 'Applied Skill',
+      typelnk: '#',
+      name: 'Node.js',
+      namelnk: '#',
+    },    
+    {
+      product: 'Python',
+      type: 'Applied Skill',
+      typelnk: '#',
+      name: 'Python 3.10',
+      namelnk: '#',
+    },    
+    {
+      product: 'Bash',
+      type: 'Applied Skill',
+      typelnk: '#',
+      name: 'Bash Scripting',
+      namelnk: '#',
+    },    
+    {
+      product: 'Serverless',
+      type: 'Applied Skill',
+      typelnk: '#',
+      name: 'AWS Lambda',
+      namelnk: '#',
+    },    
+    {
+      product: 'Serverless',
+      type: 'Applied Skill',
+      typelnk: '#',
+      name: 'GCP Cloud Functions',
+      namelnk: '#',
+    }
+  ];
