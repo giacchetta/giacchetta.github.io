15c4399e83f49261d56e4c3cadae9ee76429d48b
Luciano Giacchetta <ldgiacchetta@outlook.com>
2026-05-29 12:54:35 +0000

    Added redirects for old pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,4 +13,25 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+  redirects: {
+   '/aws-solutions-architect-professional/': '/credentials/aws-solutions-architect-professional/',
+   '/es-ar/aws-solutions-architect-professional/': '/es-ar/credentials/aws-solutions-architect-professional/',
+   '/es-uy/aws-solutions-architect-professional/': '/es-uy/credentials/aws-solutions-architect-professional/',
+   
+   '/github-actions/': '/credentials/github-actions/',
+   '/es-ar/github-actions/': '/es-ar/credentials/github-actions/',
+   '/es-uy/github-actions/': '/es-uy/credentials/github-actions/',
+   
+   '/bimodal/': '/experience/bimodal/',
+   '/es-ar/bimodal/': '/es-ar/experience/bimodal/',
+   '/es-uy/bimodal/': '/es-uy/experience/bimodal/',
+   
+   '/codyops/': '/experience/codyops/',
+   '/es-ar/codyops/': '/es-ar/experience/codyops/',
+   '/es-uy/codyops/': '/es-uy/experience/codyops/',
+   
+   '/jenkins-migration-github-argocd/': '/experience/jenkins-migration-github-argocd/',
+   '/es-ar/jenkins-migration-github-argocd/': '/es-ar/experience/jenkins-migration-github-argocd/',
+   '/es-uy/jenkins-migration-github-argocd/': '/es-uy/experience/jenkins-migration-github-argocd/',
+  },	
 });
